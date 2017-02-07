5b9fc9a26ef4541193515421562ad3db0a539732
赖依婷 <1206327437@qq.com>
2017-02-07 12:47:55 +0800

    icon

--- src/components/layout/SideBar.vue
+++ src/components/layout/SideBar.vue
@@ -21,20 +21,20 @@
       <el-submenu index="/storemanage">
         <template slot="title">仓库管理流程</template>
         <el-menu-item-group title="入库流程">
-          <el-menu-item index="/storemanage/instock"><i class="el-icon-message"></i>入库</el-menu-item>
+          <el-menu-item index="/storemanage/instock"><i class="el-icon-fa-sign-in"></i>入库</el-menu-item>
         </el-menu-item-group>
         <el-menu-item-group title="出库流程">
-          <el-menu-item index="/storemanage/outstock"><i class="el-icon-message"></i>出库</el-menu-item>
+          <el-menu-item index="/storemanage/outstock"><i class="el-icon-fa-sign-out"></i>出库</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
 
       <el-submenu index="/equipment">
         <template slot="title">设备上下架流程</template>
         <el-menu-item-group title="设备上架流程">
-          <el-menu-item index="/equipment/on"><i class="el-icon-message"></i>上架流程</el-menu-item>
+          <el-menu-item index="/equipment/on"><i class="el-icon-fa-circle"></i>上架流程</el-menu-item>
         </el-menu-item-group>
         <el-menu-item-group title="设备下架流程">
-          <el-menu-item index="/equipment/off"><i class="el-icon-message"></i>下架流程</el-menu-item>
+          <el-menu-item index="/equipment/off"><i class="el-icon-fa-circle-o"></i>下架流程</el-menu-item>
         </el-menu-item-group>
         <!-- <el-submenu index="1-4">
           <template slot="title">选项4</template>
@@ -45,27 +45,27 @@
       <el-submenu index="/system">
         <template slot="title">系统上下线流程</template>
         <el-menu-item-group title="系统上线流程">
-          <el-menu-item index="/system/online"><i class="el-icon-message"></i>上线流程</el-menu-item>
+          <el-menu-item index="/system/online"><i class="el-icon-fa-envelope"></i>上线流程</el-menu-item>
         </el-menu-item-group>
         <el-menu-item-group title="系统下线流程">
-          <el-menu-item index="/system/offline"><i class="el-icon-message"></i>下线流程</el-menu-item>
+          <el-menu-item index="/system/offline"><i class="el-icon-fa-envelope-o"></i>下线流程</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
 
       <el-submenu index="/publish">
         <template slot="title">应用发布更新流程</template>
         <el-menu-item-group title="应用发布更新流程">
-          <el-menu-item index="/publish/application"><i class="el-icon-message"></i>应用发布</el-menu-item>
+          <el-menu-item index="/publish/application"><i class="el-icon-fa-yelp"></i>应用发布</el-menu-item>
         </el-menu-item-group>
         <el-menu-item-group title="其他发布更新流程">
-          <el-menu-item index="/publish/others"><i class="el-icon-message"></i>其他发布</el-menu-item>
+          <el-menu-item index="/publish/others"><i class="el-icon-fa-bell"></i>其他发布</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
 
       <el-submenu index="/authority">
         <template slot="title">权限管理</template>
-          <el-menu-item index="/authority/user"><i class="el-icon-message"></i>用户管理</el-menu-item>
-          <el-menu-item index="/authority/role"><i class="el-icon-message"></i>角色管理</el-menu-item>
+          <el-menu-item index="/authority/user"><i class="el-icon-fa-user"></i>用户管理</el-menu-item>
+          <el-menu-item index="/authority/role"><i class="el-icon-fa-users"></i>角色管理</el-menu-item>
       </el-submenu>
 
       <el-submenu index="/others">
@@ -73,6 +73,8 @@
           <el-menu-item index="/others/worklist"><i class="el-icon-message"></i>工单管理</el-menu-item>
           <el-menu-item index="/others/manhour"><i class="el-icon-message"></i>标准工时统计</el-menu-item>
       </el-submenu>
+
+       <el-menu-item index="/part1/part13"><i class="el-icon-menu"></i>part13</el-menu-item>
 
     </el-menu>
   </aside>
