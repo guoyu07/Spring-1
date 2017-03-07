4c46aa5672851e9c2d0283de2b38159dcbe88643
tangdunwang <024216a>
2017-03-07 19:21:41 +0800

    暂存

--- src/components/part1/pages/selectForm.vue
+++ src/components/part1/pages/selectForm.vue
@@ -1,29 +1,33 @@
 <template>
   <div class="select-form">
-    <el-form :model="testForm" ref="testForm" label-width="100px">
-      <el-form-item label="字符串" prop="testStr" required>
-        <el-input v-model="testForm.testStr"></el-input>
-      </el-form-item>
-      <el-form-item label="数字" prop="testInt" required>
-        <el-input-number v-model="testForm.testInt"></el-input-number>
-      </el-form-item>
-      <el-form-item label="数组" prop="testArr" required>
-        <el-input-tag :tags="testForm.testArr"></el-input-tag>
-      </el-form-item>
-      <el-form-item label="枚举" prop="testEnum" required>
-        <el-select v-model="testForm.testEnum" placeholder="枚举类型">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="日期" prop="testDate" required>
-        <el-date-picker type="date" placeholder="选择日期" v-model="testForm.testDate"></el-date-picker>
-      </el-form-item>
-      <el-form-item label="时间" prop="testTime" required>
-        <el-time-picker type="fixed-time" placeholder="选择时间" v-model="testForm.testTime"></el-time-picker>
-      </el-form-item>
-      <el-form-item label="文本域" prop="testText" required>
-        <el-input type="textarea" v-model="testForm.testText"></el-input>
+    <el-form :model="testForm" ref="testForm" label-width="100px">  
+      <el-form-item v-for="formItem of formConfig.header"
+        v-if="['str','int','arr','enum','date','datetime','strArea'].indexOf(formItem.value.type) !== -1"
+        :label="formItem.name" :required="formItem.required">
+        <template v-if="formItem.value.type === 'str'">
+          <el-input v-model="formItem.value.regex"></el-input>
+        </template>
+        <template v-if="formItem.value.type === 'int'">
+          <el-input-number v-model="testForm.testInt"></el-input-number>
+        </template>
+        <template v-if="formItem.value.type === 'arr'">
+          <el-input-tag :tags="testForm.testArr"></el-input-tag>
+        </template>
+        <template v-if="formItem.value.type === 'enum'">
+          <el-select v-model="testForm.testEnum" placeholder="枚举类型">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </template>
+        <template v-if="formItem.value.type === 'date'">
+          <el-date-picker type="date" placeholder="选择日期" v-model="testForm.testDate"></el-date-picker>
+        </template>
+        <template v-if="formItem.value.type === 'datetime'">
+          <el-time-picker type="fixed-time" placeholder="选择时间" v-model="testForm.testTime"></el-time-picker>
+        </template>
+        <template v-if="formItem.value.type === 'strArea'">
+          <el-input type="textarea" v-model="testForm.testText"></el-input>
+        </template>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitForm">立即创建</el-button>
@@ -50,6 +54,12 @@
           testText: ''
         }
       }
+    },
+    created () {
+      this.http.get('../../../../static/formConfig.json')
+        .then(res => {
+          this.formConfig = res.data
+        })
     },
     methods: {
       submitForm () {
