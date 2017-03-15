3d5b49e751b95a83d30db09f700db7eee7297110
cherrylai <lai_yiting@126.com>
2017-03-15 18:08:21 +0800

    auth-conlicts

--- src/auth.js
+++ src/auth.js
@@ -14,12 +14,9 @@ export default {
     console.log(creds)
     context.http.post('', creds).then(response => {
       window.localStorage.setItem('userName', response.data.data.userId)
-<<<<<<< HEAD
-=======
       window.localStorage.setItem('isAdmin', response.data.data.admin)
       window.localStorage.setItem('isSuperAdmin', response.data.data.superadmin)
       window.localStorage.setItem('isProcessAdmin', response.data.data.processadmin)
->>>>>>> 7ac4a7133d459fcd7a431bc8d91625af2590d4a7
       this.authenticated = true
       // 跳转至指定目的
       if (redirect) {
