aa012beffdbf61364e15b264d89e950996f1d450
jasonlam <jasonlam0619@yahoo.com>
2017-04-01 19:50:05 +0800

    header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -26,6 +26,13 @@
       i {
         margin-right: 4px;
       }
+
+      a {
+        &:hover,
+        &.is-active {
+          text-decoration: none;
+        }
+      }
     }
   }
 </style>
@@ -33,7 +40,11 @@
 <template>
   <header class="header fw">
     <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="horizontal">
-      <el-menu-item index="1" class="logo"><b>春秋航空</b>运维管理系统</el-menu-item>
+      <el-menu-item index="1" class="logo">
+        <router-link to="/">
+          <b>春秋航空</b>运维管理系统
+        </router-link>
+      </el-menu-item>
       <el-menu-item index="3" class="fr">
         <a @click="logout">退出</a>
       </el-menu-item>
