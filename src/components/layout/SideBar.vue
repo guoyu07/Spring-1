f7224140f12261e506187226d1c26e2b4cc85ada
cherrylai <lai_yiting@126.com>
2017-04-07 23:45:11 +0800

    隐藏部分菜单

--- src/components/layout/SideBar.vue
+++ src/components/layout/SideBar.vue
@@ -54,26 +54,23 @@
       <el-menu-item-group title="仓库管理">
         <el-menu-item index="/storemanage/instock" @click="$router.go(0)"><i class="el-icon-fa-sign-in"></i>入库</el-menu-item>
         <el-menu-item index="/storemanage/outstock"><i class="el-icon-fa-sign-out"></i>出库</el-menu-item>
-        <el-menu-item index="/storemanage/outstock/edit"><i class="el-icon-fa-edit"></i>信息变更</el-menu-item>
+        <!-- <el-menu-item index="/storemanage/outstock/edit"><i class="el-icon-fa-edit"></i>信息变更</el-menu-item> -->
       </el-menu-item-group>
       <el-menu-item-group title="设备上下架">
         <el-menu-item index="/equipment/on"><i class="el-icon-fa-upload"></i>上架流程</el-menu-item>
-        <el-menu-item index="/equipment/off"><i class="el-icon-fa-download"></i>下架流程</el-menu-item>
+        <!-- <el-menu-item index="/equipment/off"><i class="el-icon-fa-download"></i>下架流程</el-menu-item> -->
       </el-menu-item-group>
       <el-menu-item-group title="系统上下线">
         <el-menu-item index="/system/applylist"><i class="el-icon-fa-envelope-open-o"></i>服务器资源申请</el-menu-item>
-        <el-menu-item index="/system/onlinelist"><i class="el-icon-star-on"></i>上线列表</el-menu-item>
-        <el-menu-item index="/system/offlinelist"><i class="el-icon-star-off"></i>下线流程</el-menu-item>
+        <!-- <el-menu-item index="/system/onlinelist"><i class="el-icon-star-on"></i>上线列表</el-menu-item> -->
+        <!-- <el-menu-item index="/system/offlinelist"><i class="el-icon-star-off"></i>下线流程</el-menu-item> -->
       </el-menu-item-group>
       <el-menu-item-group title="防火墙">
         <el-menu-item index="/firewall/apply"><i class="el-icon-fa-fire"></i>防火墙开通申请</el-menu-item>
         <el-menu-item index="/firewall/approve"><i class="el-icon-fa-gavel"></i>开通审批</el-menu-item>
         <el-menu-item index="/firewall/result"><i class="el-icon-fa-list-alt"></i>审批结果</el-menu-item>
       </el-menu-item-group>
-      <el-menu-item-group title="应用发布更新">
-        <el-menu-item index="/publish/application"><i class="el-icon-share"></i>应用发布</el-menu-item>
-        <el-menu-item index="/publish/others"><i class="el-icon-more"></i>其他发布</el-menu-item>
-      </el-menu-item-group>
+      <!--  -->
       <el-menu-item-group title="工单管理">
         <el-menu-item index="/orders"><i class="el-icon-fa-calendar-o"></i>工单管理</el-menu-item>
       </el-menu-item-group>
