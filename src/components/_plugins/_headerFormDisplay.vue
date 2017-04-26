705c9da204afc8c28fa06d6f64b08dc3a444ebb4
cherrylai <lai_yiting@126.com>
2017-04-26 19:04:56 +0800

    xxx

--- src/components/_plugins/_headerFormDisplay.vue
+++ src/components/_plugins/_headerFormDisplay.vue
@@ -1,27 +1,32 @@
 <template>
+      <!-- 新建应用时，是一个字符串，非选取的对象 -->
   <el-form-item
     :label="formItem.name">
-    <span v-if="formItem.value.type === 'dict'">
-      <!-- 新建应用时，是一个字符串，非选取的对象 -->
-      <span v-if="item && typeof item[formItem.id] === 'string'">{{ item && item[formItem.id] }}</span>
-      <span v-else>{{ item && item[formItem.id][formItem.value.source.res.show_key] }}</span>
-    </span>
-
-    <span v-else-if="formItem.value.type === 'dicts'">
-      <span v-for="span in item && item[formItem.id]">
-        {{ span[formItem.value.source.res.show_key] }}
+    <!-- {{typeof item[formItem.id]}} -->
+    <template v-if="item[formItem.id]">
+      <span v-if="formItem.value.type === 'dict'">
+        <span v-if="item && typeof item[formItem.id] === 'string'">{{ item && item[formItem.id] }}</span>
+        <span v-else>
+          {{ item && item[formItem.id][formItem.value.source.res.show_key] }}
+        </span>
       </span>
-    </span>
 
-    <span v-else-if="formItem.value.type === 'FKs'">
-      <span v-for="span in item && item[formItem.id]">{{ span.name }}</span>
-    </span>
+      <span v-else-if="formItem.value.type === 'dicts'">
+        <span v-for="span in item && item[formItem.id]">
+          {{ span[formItem.value.source.res.show_key] }}
+        </span>
+      </span>
 
-    <span v-else-if="formItem.value.type === 'arr'">
-      <span v-for="span in item && item[formItem.id]">{{ span }}</span>
-    </span>
+      <span v-else-if="formItem.value.type === 'FKs'">
+        <span v-for="span in item && item[formItem.id]">{{ span.name }}</span>
+      </span>
 
-    <span v-else>{{ item && item[formItem.id] }}</span>
+      <span v-else-if="formItem.value.type === 'arr'">
+        <span v-for="span in item && item[formItem.id]">{{ span }}</span>
+      </span>
+
+      <span v-else>{{ item && item[formItem.id] }}</span>
+    </template>
   </el-form-item>
 </template>
 
