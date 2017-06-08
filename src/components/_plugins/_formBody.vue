67188d1dfa87211733b7b0595133a0c3c7c9b2af
cherrylai <lai_yiting@126.com>
2017-06-08 10:38:25 +0800

    xxx

--- src/components/_plugins/_formBody.vue
+++ src/components/_plugins/_formBody.vue
@@ -227,8 +227,8 @@
     },
 
     mounted () {
-      // console.log('this.item: ', this.item)
-      // console.log('this.formItem: ', this.formItem)
+      console.log('this.item: ', this.item)
+      console.log('this.formItem: ', this.formItem.name, this.formItem)
       // if (this.formItem.name === '附件') console.log(this.formItem)
       // console.log(this.item[this.formItem.id])
       // console.log(this.multiFiles)
@@ -248,7 +248,6 @@
             if (this.headerTable) {
               return 'header.' + this.valueId + '.' + this.index + '.' + formItem.id
             } else if (this.bodyTable) {
-              console.log('hello', 'body.' + this.index + '.' + this.valueId + '.' + this.tableIndex + '.' + formItem.id)
               return 'body.' + this.index + '.' + this.valueId + '.' + this.tableIndex + '.' + formItem.id
             }
           }
