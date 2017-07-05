3ef2fcd4dd0daa08f4ea320c5e6cf9a2a2e1e6c7
cherrylai <lai_yiting@126.com>
2017-07-05 11:22:20 +0800

    人员选择器

---
+++ src/components/_plugins/_menberSelect.vue
@@ -0,0 +1,132 @@
+<template>
+  <div>
+    <!-- 分组 -->
+    <div v-if="strucData.isAlias">
+      <el-select
+        v-model="group"
+        :clearable="!strucData.required"
+        :allow-create="strucData.value.allow_create"
+        :disabled="strucData.readonly"
+        filterable
+        @change="groupChange"
+        placeholder="请选择">
+        <el-option
+          v-for="item in groupList"
+          :key="item.key"
+          :label="item.name"
+          :value="item">
+        </el-option>
+      </el-select>
+      <el-select
+        v-model="vmodel[strucData.id].user"
+        :clearable="!strucData.required"
+        :allow-create="strucData.value.allow_create"
+        :disabled="strucData.readonly"
+        filterable
+        placeholder="请选择">
+        <el-option
+          v-for="item in ((group && group.users) || [])"
+          :key="item.code"
+          :label="item.userId"
+          :value="item">
+        </el-option>
+      </el-select>
+    </div>
+    <!-- 不分组 -->
+    <div v-else>
+      <el-select
+        v-model="vmodel[strucData.id]"
+        :clearable="!strucData.required"
+        :allow-create="strucData.value.allow_create"
+        :disabled="strucData.readonly"
+        filterable
+        placeholder="请选择">
+        <el-tooltip
+          v-for="item in userList"
+          effect="dark"
+          :content="item.email"
+          placement="right">
+          <el-option
+            :key="item.code"
+            :label="item.userId"
+            :value="item">
+          </el-option>
+        </el-tooltip>
+      </el-select>
+      <el-button @click="assignToMe" :plain="true" type="info">分配给我</el-button>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      vmodel: { type: Object },
+      whole: { type: Object },
+      message: { type: Object },
+      strucData: { type: Object },
+      index: { type: Number },
+      tableIndex: { type: Number },
+      bodyTable: { type: Boolean },
+      headerTable: { type: Boolean }
+    },
+    data () {
+      return {
+        group: null,
+        userList: [],
+        groupList: []
+      }
+    },
+    created () {
+      if (this.strucData.isAlias) {
+        this.renderGroupList()
+      } else {
+        this.renderUserList()
+      }
+    },
+    methods: {
+      groupChange (val) {
+        this.vmodel[this.strucData.id].group = {}
+        this.vmodel[this.strucData.id].group.name = val.name
+        this.vmodel[this.strucData.id].group.key = val.key
+        this.vmodel[this.strucData.id].user = null
+      },
+      assignToMe () {
+        // console.log('分配给我')
+        const user = window.localStorage.userName
+        this.userList.map(option => {
+          if (option.userId === user) {
+            // if (Array.isArray(this.vmodel[this.strucData.id])) {
+            //   this.vmodel[this.strucData.id].push(option)
+            // } else {
+            this.vmodel[this.strucData.id] = option
+            // }
+          }
+        })
+      },
+      renderGroupList () {
+        const postHeadvData = {
+          action: 'groups/all',
+          method: 'GET',
+          data: {}
+        }
+        this.http.post('', this.parseData(postHeadvData))
+        .then((response) => {
+          console.log(response)
+          this.groupList = response.data.data
+        })
+      },
+      renderUserList () {
+        const postHeadvData = {
+          action: 'users/all',
+          method: 'GET',
+          data: {}
+        }
+        this.http.post('', this.parseData(postHeadvData))
+        .then((response) => {
+          console.log(response)
+          this.userList = response.data.data
+        })
+      }
+    }
+  }
+</script>
