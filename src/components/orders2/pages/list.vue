e683e40a76afb6989adb82af3858853709d85649
jasonlam <jasonlam0619@yahoo.com>
2017-08-10 17:36:56 +0800

    get orders from filter

--- src/components/orders2/pages/list.vue
+++ src/components/orders2/pages/list.vue
@@ -7,6 +7,7 @@
       </el-button>
       <el-table
         :data="filteredTasks.list"
+        v-loading="loading"
         border>
         <el-table-column
           v-for="col in filteredColumnList"
@@ -18,22 +19,10 @@
 </template>
 
 <script>
-  const keypath = require('keypather')()
+  import getFilteredTasks from './../../../mixins/getFilteredTasks'
 
   export default {
-    data () {
-      return {
-        filterData: {},
-        filteredTasks: {},
-        filteredColumnList: []
-      }
-    },
-
-    computed: {
-      orderId () {
-        return this.$route.params.id
-      }
-    },
+    mixins: [ getFilteredTasks ],
 
     watch: {
       '$route.params.id' (id) {
@@ -43,60 +32,6 @@
 
     mounted () {
       this.getFilterData(this.orderId)
-    },
-
-    methods: {
-      getFilterData (id) {
-        let postData = {
-          action: 'filter',
-          method: 'GET',
-          data: { id }
-        }
-        this.http.post('/flow/', this.parseData(postData)).then((res) => {
-          if (res.status === 200) {
-            this.filterData = res.data.data
-            this.getFilteredTasks()
-          }
-        })
-      },
-
-      getFilteredTasks () {
-        let { filters, order } = this.filterData
-        let postData = {
-          action: 'filter/tasks',
-          method: 'GET',
-          data: {
-            filters,
-            order
-          }
-        }
-        this.http.post('/flow/', this.parseData(postData)).then((res) => {
-          if (res.status === 200) {
-            this.filteredTasks = res.data.data
-            this.getColumnList()
-          }
-        })
-      },
-
-      getColumnList () {
-        let postData = {
-          action: 'filter/show/fields',
-          method: 'GET',
-          data: {}
-        }
-        this.http.post('/flow/', this.parseData(postData)).then((res) => {
-          if (res.status === 200) {
-            const columnList = res.data.data.list
-            this.filteredColumnList = columnList.filter(col => this.filteredTasks.list.some(task => keypath.get(task, col.key_path) !== undefined))
-            // columnList.forEach((col) => {
-            //   // console.log(this.filteredTasks.list.some(task => _._byString(task, col.key_path)))
-            //   this.filteredTasks.list.forEach((task) => {
-            //     console.log(keypath.get(task, col.key_path))
-            //   })
-            // })
-          }
-        })
-      }
     }
   }
 </script>
