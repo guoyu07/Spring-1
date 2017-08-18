1d3ecce81903994e3137d2db4697be2888bdf1a2
jasonlam <jasonlam0619@yahoo.com>
2017-08-18 11:28:46 +0800

    finished order

--- src/components/orders/pages/_plugins/_taskDialog.vue
+++ src/components/orders/pages/_plugins/_taskDialog.vue
@@ -6,37 +6,37 @@
     <el-row>
       <el-col :span="20" :offset="2">
         <el-form label-position="left" inline class="expanded-form" label-width="100px">
-          <el-form-item v-if="taskViewData.order.pinstance.pd.pname" label="任务名称：">
+          <el-form-item v-if="taskViewData.order.pinstance.pd.pname" label="任务名称">
             <span>{{taskViewData.order.pinstance.pd.pname}}</span>
           </el-form-item>
-          <el-form-item v-if="taskViewData.order.pinstance.pd.pname" label="所属流程：">
+          <el-form-item v-if="taskViewData.order.pinstance.pd.pname" label="所属流程">
             <span>{{taskViewData.order.pinstance.pd.pname}}</span>
           </el-form-item>
-          <el-form-item v-if="taskViewData.order.pinstance.pnum" label="流程单号：">
+          <el-form-item v-if="taskViewData.order.pinstance.pnum" label="流程单号">
             <span>{{taskViewData.order.pinstance.pnum}}</span>
           </el-form-item>
-          <el-form-item v-if="taskViewData.order.pinstance.author" label="发起者：">
+          <el-form-item v-if="taskViewData.order.pinstance.author" label="发起者">
             <span>{{taskViewData.order.pinstance.author.nick}}</span>
           </el-form-item>
-          <el-form-item v-if="taskViewData.order.assign" label="处理人：">
+          <el-form-item v-if="taskViewData.order.assign" label="处理人">
             <span>{{taskViewData.order.assign.nick}}</span>
           </el-form-item>
-          <el-form-item v-if="taskViewData.order.assign_group" label="处理组：">
+          <el-form-item v-if="taskViewData.order.assign_group" label="处理组">
             <span>{{taskViewData.order.assign_group.name}}</span>
           </el-form-item>
-          <el-form-item v-if="taskViewData.order.candidate_users.length" label="候选人：">
+          <el-form-item v-if="taskViewData.order.candidate_users.length" label="候选人">
             <el-tag v-for="user in taskViewData.order.candidate_users" type="gray">{{user.nick}}</el-tag>
           </el-form-item>
-          <el-form-item v-if="taskViewData.order.candidate_groups.length" label="候选人：">
+          <el-form-item v-if="taskViewData.order.candidate_groups.length" label="候选人">
             <el-tag v-for="group in taskViewData.order.candidate_groups" type="gray">{{group.name}}</el-tag>
           </el-form-item>
-          <el-form-item v-if="taskViewData.order.ctime" label="创建时间：">
+          <el-form-item v-if="taskViewData.order.ctime" label="创建时间">
             <span>{{taskViewData.order.ctime}}</span>
           </el-form-item>
-          <el-form-item v-if="taskViewData.order.assign_time" label="认领时间：">
+          <el-form-item v-if="taskViewData.order.assign_time" label="认领时间">
             <span>{{taskViewData.order.assign_time}}</span>
           </el-form-item>
-          <el-form-item v-if="taskViewData.order.etime" label="结束时间：">
+          <el-form-item v-if="taskViewData.order.etime" label="结束时间">
             <span>{{taskViewData.order.etime}}</span>
           </el-form-item>
         </el-form>
@@ -49,16 +49,16 @@
         <el-collapse v-if="taskViewData.order.pinstance.history_list.length" class="history-list">
           <el-collapse-item v-for="(task, key) in taskViewData.order.pinstance.history_list" :title="(key + 1).toString() + '. ' + task.name">
             <el-form label-position="left" label-width="90px" inline class="expanded-form">
-              <el-form-item v-if="task.task_key" label="任务 Key：">
+              <el-form-item v-if="task.task_key" label="任务 Key">
                 <code>{{task.task_key}}</code>
               </el-form-item>
-              <el-form-item v-if="task.operator" label="操作者：">
+              <el-form-item v-if="task.operator" label="操作者">
                 <span>{{task.operator.nick}}</span>
               </el-form-item>
-              <el-form-item v-if="task.time" label="时间：">
+              <el-form-item v-if="task.time" label="时间">
                 <span>{{task.time}}</span>
               </el-form-item>
-              <el-form-item v-if="task.workFlowNo" label="工单号：">
+              <el-form-item v-if="task.workFlowNo" label="工单号">
                 <span>{{task.workFlowNo}}</span>
               </el-form-item>
             </el-form>
