f62f375e215effe6dd1f05c9d64e3f57c3e51657
morance <869127242@qq.com>
2017-08-30 18:17:29 +0800

    account pages basics

---
+++ src/components/account/pages/basics.vue
@@ -0,0 +1,92 @@
+<template>
+ <div>
+    <el-row>
+     <el-col :md="24" :lg="20">
+     <el-card>
+      <h3><i class="el-icon-setting"></i>设置基本信息</h3>
+      <el-form :model='basicsForm' ref='basicsForm' :rules="basicsCheck" >
+      <el-form-item label="用户名：" >
+       <el-input v-model='basicsForm.userId' disabled></el-input>
+      </el-form-item>
+      <el-form-item label="昵称：" >
+       <el-input v-model="basicsForm.nick" ></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱：" prop="email">
+       <el-input v-model="basicsForm.email" ></el-input>
+      </el-form-item>
+      <el-form-item label="手机：" prop="phone">
+       <el-input v-model="basicsForm.phone" ></el-input>
+      </el-form-item>
+      <el-button type="primary" @click="submitForm('basicsForm')">保存</el-button>
+      </el-form>
+     </el-card>
+     </el-col>
+    </el-row>
+ </div>
+</template>
+<script>
+export default {
+  data () {
+    var phoneCheck = (rule, value, callback) => {
+      let reg = /^1[34578]\d{9}$/
+      if (value === '') {
+        callback()
+      } else {
+        if (!reg.test(value)) {
+          return callback(new Error('请输入正确的手机号'))
+        } else { callback() }
+      }
+    }
+    return {
+      test: '',
+      basicsForm: {
+        userId: '',
+        nick: '',
+        email: '',
+        phone: ''
+      },
+      basicsCheck: {
+        email: [{ type: 'email', message: '请输入正确的邮箱地址', trigger: 'change' }],
+        phone: [{validator: phoneCheck, trigger: 'change'}]
+      }
+    }
+  },
+  created () {
+    this.basicsForm.userId = this.$store.state.userinfo.userId
+    this.basicsForm.nick = this.$store.state.userinfo.nick
+    this.basicsForm.email = this.$store.state.userinfo.email
+    this.basicsForm.phone = this.$store.state.userinfo.phone
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.updateUserMessage()
+        } else {
+          return false
+        }
+      })
+    },
+    updateUserMessage () {
+      let postData = {
+        action: 'user',
+        method: 'put',
+        data: {
+          userId: this.basicsForm.userId,
+          nick: this.basicsForm.nick,
+          email: this.basicsForm.email,
+          phone: this.basicsForm.phone
+        }
+      }
+      this.http.post('/user/', this.parseData(postData)).then((res) => {
+        if (res.status === 200) {
+          this.$message({message: '修改成功', type: 'success', duration: '1000'})
+          this.$store.dispatch('update_userinfo', {
+            userinfo: res.data.data
+          })
+        }
+      })
+    }
+  }
+}
+</script>
