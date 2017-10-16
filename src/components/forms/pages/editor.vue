6f7221f7eff88bb475c14c07b8ca9688bda9db94
jasonlam <jasonlam0619@yahoo.com>
2017-10-16 11:55:11 +0800

    auto fill form conf

--- src/components/forms/pages/editor.vue
+++ src/components/forms/pages/editor.vue
@@ -126,8 +126,11 @@
               </template>
 
               <el-form-item width="100%" label="额外配置">
-                <el-checkbox v-model="formConfig.show_progress">是否显示进度条</el-checkbox>
-                <el-checkbox v-model="formConfig.show_history">是否显示步骤</el-checkbox>
+                <el-checkbox v-model="formConfig.show_progress">显示进度条</el-checkbox>
+                <el-checkbox v-model="formConfig.show_history">显示步骤</el-checkbox>
+                <el-checkbox
+                   v-if="$route.query.tkey !== 'start'"
+                   v-model="formConfig.fill_form">自动填充表单</el-checkbox>
               </el-form-item>
 
               <!-- <template v-if="formConfig.action.find(_ => _.type !== 'target')">
